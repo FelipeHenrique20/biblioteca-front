70fc42ce9af28bb0858c667c77f6da129134eef5
Felipe Henrique <fiphg49@gmail.com>
2026-08-26 15:07:09 -0300

    style: aprimorar página de autores

--- src/pages/Autores.tsx
+++ src/pages/Autores.tsx
@@ -1,7 +1,13 @@
 import AutorSection from "../components/AutorSection";
 
 function Autores() {
-    return <AutorSection />;
+    return (
+        <>
+        <h1>Autores</h1>
+        <p className="subtitulo">Gerencie os autores cadastrados na biblioteca</p>
+        <AutorSection />
+        </>
+    );
 }
 
 export default Autores;
