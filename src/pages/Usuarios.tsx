a76499d966134bc643232404abdb7ffe0a4e9648
Felipe Henrique <fiphg49@gmail.com>
2026-08-26 15:07:45 -0300

    style: aprimorar página de usuários

--- src/pages/Usuarios.tsx
+++ src/pages/Usuarios.tsx
@@ -1,7 +1,13 @@
 import UsuarioSection from "../components/UsuarioSection";
 
 function Usuarios() {
-    return <UsuarioSection />;
+    return (
+        <>
+        <h1>Usuarios</h1>
+        <p className="subtitulo">Gerencie os usuários cadastrados</p>
+        <UsuarioSection />
+        </>
+    );
 }
 
 export default Usuarios;
