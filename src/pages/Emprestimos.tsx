066f8c95da7835fa1bf232f8c65f6b63b551c115
Felipe Henrique <fiphg49@gmail.com>
2026-08-26 15:07:26 -0300

    style: aprimorar página de empréstimos

--- src/pages/Emprestimos.tsx
+++ src/pages/Emprestimos.tsx
@@ -1,7 +1,13 @@
 import EmprestimoSection from "../components/EmprestimoSection";
 
 function Emprestimos() {
-    return <EmprestimoSection />;
+    return (
+        <>
+        <h1>Emprestimos</h1>
+        <p className="subtitulo">Registre e acompanhe empréstimos ativos</p>
+        <EmprestimoSection />
+        </>
+    );
 }
 
 export default Emprestimos;
